e0126636b4b37eae18df4fa7516b328632914dce
Brent Gulanowski <bgulanowski@gmail.com>
2015-11-30 09:45:16 -0500

    Add Grove class representing a group of four grids, one for each sector.

--- Cellulon/Grid.swift
+++ Cellulon/Grid.swift
@@ -22,6 +22,19 @@ public func -(lhs: PointI, rhs: PointI) -> PointI {
     return PointI(x: lhs.x - rhs.x, y: lhs.y - rhs.y)
 }
 
+public func *(lhs: PointI, rhs: Int) -> PointI {
+    return PointI(x: lhs.x * rhs, y: lhs.y * rhs)
+}
+
+// Cross product and Dot product don't make sense for Integer Points
+public func *(lhs: PointI, rhs: PointI) -> PointI {
+    return PointI(x: lhs.x * rhs.x, y: lhs.y * rhs.y)
+}
+
+public func abs(lhs: PointI) -> PointI {
+    return PointI(x: abs(lhs.x), y: abs(lhs.y))
+}
+
 public class Point<T> {
 
     let x: T
@@ -36,7 +49,7 @@ public class Point<T> {
 public typealias GridPoint = PointI
 
 public protocol Grid_ {
-    func valueAtPoint(point: GridPoint) -> Int?
+    func valueAtPoint(point: GridPoint) -> Int
     func setValue(value: Int, atPoint point: GridPoint) -> Void
 }
 
@@ -48,6 +61,10 @@ public extension Grid_ {
     
     final func pointForIndex(index: Int, dim: Int) -> GridPoint {
         return GridPoint(x: index%dim, y: index/dim)
+    }
+    
+    public func sectorForPoint(point: GridPoint, dim: Int) -> Int {
+        return (point.x < dim ? 0 : 1) + (point.y < dim ? 0 : 2)
     }
 }
 
@@ -63,9 +80,9 @@ public class Grid : Grid_ {
         self.val = [Int](count: siz, repeatedValue: 0)
     }
     
-    public final func valueAtPoint(point: GridPoint) -> Int? {
+    public final func valueAtPoint(point: GridPoint) -> Int {
         let index = indexForPoint(point, dim: dim)
-        return index < siz ? val[index] : nil
+        return index < siz ? val[index] : 0
     }
     
     public final func setValue(value: Int, atPoint point: GridPoint) -> Void {
@@ -99,31 +116,29 @@ public class GridTree : Tree<Grid_> {
     
     let dim: Int
     let lev: Int
-    let par: GridTree
+    let par: GridTree?
     
-    init(par: GridTree, dim: Int, lev: Int) {
+    init(dim: Int, par: GridTree?, lev: Int) {
         self.par = par
         self.dim = dim
         self.lev = lev
         super.init(root: nil, limbs: [Int : Grid_]())
     }
     
-    public func indexForPoint(point: GridPoint) -> Int {
-        return (point.x > dim ? 0 : 1) + (point.y > dim ? 2 : 4)
+    convenience init(dim: Int) {
+        self.init(dim: dim, par: nil, lev: 0)
     }
     
     public func offsetForIndex(index: Int) -> Int {
         return dim * dim * index
     }
     
-    public func limbOriginForIndex(index: Int) -> GridPoint {
-        return PointI(x: (index & 1) * dim, y: (index & 2) * dim)
+    public func originForSector(sector: Int) -> GridPoint {
+        return PointI(x: (sector & 1) * dim, y: (sector & 2) * dim)
     }
     
     public func reverseOffsetPoint(point: GridPoint) -> GridPoint {
-        let index = indexForPoint(point)
-        let offset = limbOriginForIndex(index)
-        return point - offset
+        return point - originForSector(sectorForPoint(point, dim: dim))
     }
     
     public final func limbForPoint(point: Point<Int>) -> Grid_? {
@@ -133,12 +148,12 @@ public class GridTree : Tree<Grid_> {
 
 extension GridTree : Grid_ {
     
-    public final func valueAtPoint(point: Point<Int>) -> Int? {
+    public final func valueAtPoint(point: Point<Int>) -> Int {
         if let limb = limbForPoint(point) {
             return limb.valueAtPoint(point)
         }
         else {
-            return nil
+            return 0
         }
     }
     
@@ -148,14 +163,75 @@ extension GridTree : Grid_ {
             limb = l
         }
         else {
-            if lev > 0 {
-                limb = Grid(dim: dim)
-            }
-            else {
-                limb = GridTree(par: self, dim: dim, lev: lev-1)
-            }
-            limbs[indexForPoint(point)] = limb
+            limb = newLimb()
+            limbs[sectorForPoint(point, dim: dim)] = limb
         }
         limb.setValue(value, atPoint: point)
+    }
+    
+    final func newBranch() -> GridTree {
+        return GridTree(dim: dim, par: self, lev: lev-1)
+    }
+    
+    final func newLeaf() -> Grid {
+        return Grid(dim: dim)
+    }
+    
+    final func newLimb() -> Grid_ {
+        return lev == 0 ? newLeaf() : newBranch()
+    }
+}
+
+public class Grove: Grid_ {
+    
+    let dim: Int
+    var trees = [ Int : GridTree ]()
+    
+    init(dim: Int) {
+        self.dim = dim
+    }
+    
+    public final func valueAtPoint(point: GridPoint) -> Int {
+        if let tree = trees[sectorForPoint(point)] {
+            return tree.valueAtPoint(abs(point))
+        }
+        else {
+            return 0
+        }
+    }
+    
+    public final func setValue(value: Int, atPoint point: GridPoint) -> Void {
+        if let tree = trees[sectorForPoint(point)] {
+            tree.setValue(value, atPoint: abs(point))
+        }
+    }
+    
+    final func sectorForPoint(point: GridPoint) -> Int {
+        // treat negative values as > dim
+        return sectorForPoint(point, dim: 0)
+    }
+    
+    final func transformPoint(point: GridPoint, forSector sector: Int) -> GridPoint {
+        return point * transformForSector(sector)
+    }
+    
+    final func transformForSector(sector: Int) -> GridPoint {
+        return GridPoint(x: sector & 1 == 0 ? 1 : -1, y: sector & 2 == 0 ? 1 : -1)
+    }
+    
+    final func treeInSector(sector: Int) -> GridTree? {
+        return (trees[sector] != nil) ? trees[sector] : newTreeInSector(sector)
+    }
+    
+    final func newTreeInSector(sector: Int) -> GridTree? {
+        if sector < 4 {
+            let tree = GridTree(dim: dim)
+            trees[sector] = tree
+            return tree
+        }
+        else {
+            // FIXME: throw exception
+            return nil
+        }
     }
 }
