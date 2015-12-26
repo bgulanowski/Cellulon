bbd489571e5e0c44a49eae193b6d6e8dc2ddc3cf
Brent Gulanowski <bgulanowski@gmail.com>
2015-12-26 10:51:52 -0500

    Move PatternCell to new file.

---
+++ Cellulon/PatternCell.swift
@@ -0,0 +1,42 @@
+//
+//  PatternCell.swift
+//  Cellulon
+//
+//  Created by Brent Gulanowski on 2015-12-26.
+//  Copyright © 2015 Lichen Labs. All rights reserved.
+//
+
+import UIKit
+
+class PatternCell: UITableViewCell {
+    
+    var ruleKeeper: RuleKeeper!
+    
+    @IBOutlet var bit0View: BitView!
+    @IBOutlet var bit1View: BitView!
+    @IBOutlet var bit2View: BitView!
+    
+    @IBOutlet var label: UILabel!
+    @IBOutlet var enabledSwitch: UISwitch!
+    
+    var number: Int = 0 {
+        didSet {
+            label.text = "\(number)"
+            enabledSwitch.on = ruleKeeper.rule.bit(number)
+            bit0View.enabled = (number & 1 == 1)
+            bit1View.enabled = (number & 2 == 2)
+            bit2View.enabled = (number & 4 == 4)
+        }
+    }
+    
+    @IBAction func updateEnabled(sender: UISwitch) {
+        var rule = ruleKeeper.rule
+        if sender.on {
+            rule.setBit(number)
+        }
+        else {
+            rule.clearBit(number)
+        }
+        ruleKeeper!.rule = rule
+    }
+}
--- Cellulon/Settings.swift
+++ Cellulon/Settings.swift
@@ -147,36 +147,3 @@ class BitFlagSection: TableSection, RuleKeeper {
         return cell
     }
 }
-
-class PatternCell: UITableViewCell {
-    
-    var ruleKeeper: RuleKeeper!
-    
-    @IBOutlet var bit0View: BitView!
-    @IBOutlet var bit1View: BitView!
-    @IBOutlet var bit2View: BitView!
-    
-    @IBOutlet var label: UILabel!
-    @IBOutlet var enabledSwitch: UISwitch!
-    
-    var number: Int = 0 {
-        didSet {
-            label.text = "\(number)"
-            enabledSwitch.on = ruleKeeper.rule.bit(number)
-            bit0View.enabled = (number & 1 == 1)
-            bit1View.enabled = (number & 2 == 2)
-            bit2View.enabled = (number & 4 == 4)
-        }
-    }
-    
-    @IBAction func updateEnabled(sender: UISwitch) {
-        var rule = ruleKeeper.rule
-        if sender.on {
-            rule.setBit(number)
-        }
-        else {
-            rule.clearBit(number)
-        }
-        ruleKeeper!.rule = rule
-    }
-}
