b969ce70c9e05eda6308a56714324613795a52f7
Brent Gulanowski <bgulanowski@gmail.com>
2016-01-08 09:49:02 -0500

    Still working on getting render-to-texture CA to work.
    Create a framebuffer and textures to act as drawing source/target, then render that texture to main view framebuffer. Testing drawing into textures but only one is updating. glDrawBuffers() is not doing what I expect? Missing something?

--- Cellulon/Auto2GLView.swift
+++ Cellulon/Auto2GLView.swift
@@ -14,18 +14,36 @@ class Auto2GLView: UIView {
     
     var context: EAGLContext!
     var framebuffer: Framebuffer!
-
     var program: Program!
+    
     var pointBuffer: Point2Buffer!
     var textures = [Texture]()
+
+    var textureFB: Framebuffer!
+    var textureProg: Program!
+    var texCoordBuffer: Point2Buffer!
+    var reverse = false
+    
+    var displayLink: CADisplayLink!
     
     func makePoints() -> Point2Buffer {
         let elements = [
-            Point2(tuple: (-0.5, -0.5)),
-            Point2(tuple: ( 0.5,  0.5)),
-            Point2(tuple: (-0.5,  0.5))
+            Point2(tuple: (-1.0, -1.0)),
+            Point2(tuple: ( 1.0, -1.0)),
+            Point2(tuple: ( 1.0,  1.0)),
+            Point2(tuple: (-1.0,  1.0))
         ]
         return Point2Buffer(elements: elements)
+    }
+    
+    func makeTexCoords() -> TexCoordBuffer {
+        let elements = [
+            TexCoord(tuple: (0.0, 0.0)),
+            TexCoord(tuple: (0.0, 1.0)),
+            TexCoord(tuple: (1.0, 1.0)),
+            TexCoord(tuple: (1.0, 0.0)),
+        ]
+        return TexCoordBuffer(elements: elements)
     }
     
     var glLayer: CAEAGLLayer {
@@ -39,7 +57,11 @@ class Auto2GLView: UIView {
     override func didMoveToSuperview() {
         super.didMoveToSuperview()
         prepareGL()
-        render()
+        displayLink = CADisplayLink(target: self, selector: "render")
+        displayLink.addToRunLoop(NSRunLoop.mainRunLoop(), forMode: NSRunLoopCommonModes)
+        displayLink.paused = false
+        displayLink.frameInterval = 15
+//        render()
     }
     
     func prepareGL() {
@@ -48,6 +70,7 @@ class Auto2GLView: UIView {
         
         // these can happen in any order
         prepareDrawable()
+        prepareTextureFramebuffer()
         prepareRendererState()
         prepareShaders()
         prepareContent()
@@ -59,22 +82,33 @@ class Auto2GLView: UIView {
     }
     
     func prepareDrawable() {
-        // These lines must happen in this exact order
         let colorBuffer = Renderbuffer()
+        // renderbuffer storage must be allocated before it is attached to the framebuffer
         context.renderbufferStorage(colorBuffer, fromDrawable: glLayer)
         framebuffer = Framebuffer()
-        framebuffer.colorAttachment0 = colorBuffer
+        framebuffer.setColorAttachment(colorBuffer, atIndex: 0)
+    }
+    
+    func prepareTextureFramebuffer() {
+        textureFB = Framebuffer()
+//        let texture0 = Texture(size: CGSize(width: 256, height: 256), data: nil)
+        let texture0 = Texture.textureWithName("David med", filetype: "png")
+        let texture1 = Texture(size: CGSize(width: 256, height: 256), data: nil)
+        textureFB.setColorAttachment(texture0, atIndex: 0)
+        textureFB.setColorAttachment(texture1, atIndex: 1)
+        if glCheckFramebufferStatus(GLenum(GL_FRAMEBUFFER)) != GLenum(GL_FRAMEBUFFER_COMPLETE) {
+            print("texture framebuffer is not complete")
+        }
     }
     
     func prepareShaders() {
-        program = Program.newProgramWithName("Conway")
+        program = Program()
+        textureProg = Program.newProgramWithName("Conway")
     }
     
     func prepareContent() {
-        if let texture = Texture.textureWithName("David", filetype: "png") {
-            textures.append(texture)
-        }
         pointBuffer = makePoints()
+        texCoordBuffer = makeTexCoords()
     }
     
     func prepareRendererState() {
@@ -83,13 +117,52 @@ class Auto2GLView: UIView {
         glClearColor(0.5, 0, 0, 1)
     }
     
+    func update() {
+        
+        textureFB.bind()
+        var drawBuffer = GLenum(reverse ? GL_COLOR_ATTACHMENT0 : GL_COLOR_ATTACHMENT1)
+        glDrawBuffers(1, &drawBuffer)
+        
+        textureProg.use()
+        let texture = textureFB.colorAttachmentAtIndex(reverse ? 1 : 0)
+
+        textureProg.submitTexture(texture as! Texture, uniformName: "sampler")
+        textureProg.submitBuffer(pointBuffer, name: "position")
+        textureProg.submitBuffer(texCoordBuffer, name: "texCoord")
+        
+        glDrawArrays(GLenum(GL_TRIANGLE_FAN), 0, pointBuffer.count)
+        glFinish()
+
+        reverse = !reverse
+    }
+    
     func render() {
+        
+        framebuffer.bind()
         glClear(GLbitfield(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT))
+        
         program.use()
+        
+        // TODO: support for matrices in Program.submitUniformMatrix
+        let matrix = Array<GLfloat>( arrayLiteral:
+            1.0, 0.0, 0.0, 0.0,
+            0.0, 1.0, 0.0, 0.0,
+            0.0, 0.0, 1.0, 0.0,
+            0.0, 0.0, 0.0, 1.0
+        )
+        
+        glUniformMatrix4fv(program.getLocationOfUniform("MVP"), 1, GLboolean(GL_FALSE), matrix)
+
+        let texture = textureFB.colorAttachmentAtIndex(reverse ? 1 : 0)
+        program.submitUniform(GLuint(GL_TRUE), uniformName: "useTex")
+        program.submitTexture(texture as! Texture, uniformName: "sampler")
         program.submitBuffer(pointBuffer, name: "position")
-        if textures.count > 0 {
-            program.submitTexture(textures[0], uniformName: "s_texture")
-        }
+        program.submitBuffer(texCoordBuffer, name: "texCoord")
+
+        glDrawArrays(GLenum(GL_TRIANGLE_FAN), 0, pointBuffer.count)
+
         self.context.presentRenderbuffer(Int(GL_FRAMEBUFFER))
+        
+        update()
     }
 }
--- Cellulon/Conway.fp
+++ Cellulon/Conway.fp
@@ -2,11 +2,11 @@
 
 precision highp float;
 
-uniform sampler2D s_texture;
+uniform sampler2D sampler;
 
 in vec2 point;
 
-out vec4 vFragColor;
+layout (location = 0) out lowp vec4 vFragColor;
 
 float rnd(vec2 x) {
     int n = int(x.x * 400.0 + x.y * 64000.0);
@@ -14,9 +14,57 @@ float rnd(vec2 x) {
     return 1.0 - float( (n * (n * n * 15731 + 789221) + 1376312589) & 0x7fffffff) / 1073741824.0;
 }
 
+int count(vec2 p) {
+    
+    const vec2 neighbours[8] = vec2[](
+        vec2(-1, -1),
+        vec2(-1,  0),
+        vec2(-1,  1),
+        
+        vec2( 0, -1),
+        vec2( 0,  1),
+        
+        vec2( 1, -1),
+        vec2( 1,  0),
+        vec2( 1,  1)
+    );
+    
+    int count = 0;
+    for (int i = 0; i < 8; ++i) {
+        if (texture(sampler, p + neighbours[i]).r > 0.5) {
+            ++count;
+        }
+    }
+    return count;
+}
+
+// TODO: convert to uniforms
+const int min = 2;
+const int max = 3;
+const int add = 3;
+
+const vec4 alive = vec4(1.0);
+const vec4 dead = vec4(vec3(0.0), 1.0);
+
 void main() {
+
+    vec4 texColor = texture(sampler, point);
+#if 1
     float random = rnd(point) > 0.5 ? 1.0 : 0.0;
-    vec4 texColor = texture(s_texture, point);
     vec4 randomColor = vec4(random, random, random, 1.0);
     vFragColor = texColor * 0.6 + randomColor * .4;
+#else
+    vFragColor = texColor;
+    int count = count(point);
+    
+    if (count < min || count > max) {
+        vFragColor = dead;
+    }
+    else if (count == add) {
+        vFragColor = alive;
+    }
+    else {
+        vFragColor = texColor;
+    }
+#endif
 }
