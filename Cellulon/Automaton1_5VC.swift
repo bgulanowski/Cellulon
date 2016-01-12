316dc8e097da36d02b051b037bda6c45a3f8bf13
Brent Gulanowski <bgulanowski@gmail.com>
2016-01-11 20:16:52 -0500

    Add support for hiding nav bar and saving images with tap gestures.

--- Cellulon/Automaton1_5VC.swift
+++ Cellulon/Automaton1_5VC.swift
@@ -10,7 +10,7 @@ import UIKit
 
 let settingsSegueID = "settings"
 
-class Automaton1_5VC: UIViewController {
+class Automaton1_5VC: UIViewController, UIGestureRecognizerDelegate {
     
     var rule: Rule = 165
     
@@ -19,7 +19,10 @@ class Automaton1_5VC: UIViewController {
     private var settings: Settings?
     private var edgesWrap = false
     private var firstGen = FirstGeneration.Default
-    
+
+    var singleTapGR: UITapGestureRecognizer!
+    var doubleTapGR: UITapGestureRecognizer!
+
     @IBOutlet weak var imageView: UIImageView!
     
     // MARK: UIViewController
@@ -28,6 +31,12 @@ class Automaton1_5VC: UIViewController {
         super.viewDidLoad()
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Settings", style: .Plain, target: self, action: "showSettings:")
         imageView.layer.magnificationFilter = "nearest"
+        singleTapGR = UITapGestureRecognizer(target: self, action: "toggleBars:")
+        doubleTapGR = UITapGestureRecognizer(target: self, action: "saveImage:")
+        doubleTapGR.numberOfTapsRequired = 2
+        singleTapGR.requireGestureRecognizerToFail(doubleTapGR)
+        imageView.addGestureRecognizer(singleTapGR)
+        imageView.addGestureRecognizer(doubleTapGR)
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -56,10 +65,20 @@ class Automaton1_5VC: UIViewController {
         }
     }
     
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+    
+    // MARK: UIGestureRecognizerDelegate
+    
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return otherGestureRecognizer == singleTapGR || otherGestureRecognizer == doubleTapGR
+    }
+    
     // MARK: New
     
     func showFirstTime() {
-        let size = imageView.bounds.size
+        let size = view.bounds.size
         updateWithRule(rule, width: Int(size.width), height: Int(size.height), firstGen: .Default, edgesWrap: false)
     }
 
@@ -79,6 +98,37 @@ class Automaton1_5VC: UIViewController {
     // MARK: Actions
     func showSettings(sender: UIBarButtonItem) {
         self.performSegueWithIdentifier(settingsSegueID, sender: sender)
+    }
+    
+    @IBAction func toggleBars(sender: UITapGestureRecognizer) {
+        if let nav = self .navigationController {
+            let hidden = nav.navigationBarHidden
+            nav.setNavigationBarHidden(!hidden, animated: true)
+        }
+    }
+    
+    @IBAction func saveImage(sender: UITapGestureRecognizer) {
+        let alert = UIAlertController(title: "Save Image", message: "Where would you like to save the image?", preferredStyle: .Alert)
+        let photosAction = UIAlertAction(title: "Photo Library", style: .Default, handler: { (action: UIAlertAction) in
+            // TODO:
+        })
+        photosAction.enabled = false
+        alert.addAction(photosAction)
+        alert.addAction(UIAlertAction(title: "Documents", style: .Default, handler: { (action: UIAlertAction) in
+            self.saveImageToDocuments()
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    func saveImageToDocuments() {
+        if let image = imageView.image {
+            if let directory = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true).first {
+                let url = NSURL(fileURLWithPath: directory).URLByAppendingPathComponent("Rule \(rule).png")
+                UIImagePNGRepresentation(image)?.writeToURL(url, atomically: true)
+                print("saved image to %@", url)
+            }
+        }
     }
 }
 
